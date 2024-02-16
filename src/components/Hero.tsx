3b6ebe92032ba5341a7f36828001ef486ea5c02d
Jhan Gale Garcia <garcia.johngale@gmail.com>
2024-02-16 13:09:57 +0800

    fix(hero): proper font styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,14 @@ export function Hero() {
 				<main>
 					<section>
 						<h1 className="text-6xl font-bold leading-tight">
-							Hey, I&apos;m <span className="text-blurple">John</span>!
+							Hey, I&apos;m <span className="text-sky-500">John</span>!
 						</h1>
-						<p className="py-4 text-lg font-[400] prose text-black dark:text-white">
-							I am a 23-year-old <span className="font-bold text-blurple">software developer</span> and a{" "}
-							<span className="font-bold text-blurple">discord bot developer</span> from the Philippines with a passion
-							for completing my studies and pursuing my career. Get in touch with me via my socials below or check out
-							my portfolio.
+						<p className="py-4 text-lg font-light prose text-neutral-800 dark:text-neutral-300">
+							I am a 23 year old{" "}
+							<span className="font-bold text-neutral-900 dark:text-neutral-200">software developer</span> and a{" "}
+							<span className="font-bold text-neutral-900 dark:text-neutral-200">discord bot developer</span> from
+							Philippines with a passion for completing my studies and pursuing my career. Get in touch with me via my
+							socials below or check out my portfolio.
 						</p>
 						<div className="flex flex-row gap-4">
 							<Link
@@ -28,7 +29,7 @@ export function Hero() {
 							<Link
 								href="/contact"
 								className={cn(
-									"btn rounded bg-white dark:bg-dark border-white dark:border-dark hover:bg-white hover:border-dark dark:hover:bg-dark dark:hover:border-white text-black dark:text-white transition-colors",
+									"btn rounded bg-white dark:bg-dark border-neutral-400 dark:border-neutral-700 hover:bg-white hover:border-dark dark:hover:bg-dark dark:hover:border-white text-black dark:text-white transition-colors",
 								)}
 							>
 								Contact Me
