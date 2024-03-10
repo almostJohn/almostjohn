f7e38dac04598cdea649d132bcabb7ccf312debb
Jhan Gale Garcia <garcia.johngale@gmail.com>
2024-03-10 10:24:36 +0800

    rewrite: contents on the home page

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
-import {
-	SiNextdotjs as Nextjs,
-	SiReact as ReactIcon,
-	SiTypescript as TypeScript,
-	SiPostgresql as PostgreSQL,
-} from "@icons-pack/react-simple-icons";
+import { SiNextdotjs as Nextjs, SiReact as ReactIcon } from "@icons-pack/react-simple-icons";
 import { buttonVariants } from "./ui/button";
 import { cn } from "~/lib/utils";
 
@@ -17,7 +12,8 @@ export function HeroSection() {
 					<div className="space-y-3">
 						<h1 className="text-3xl mb-8 tracking-tighter">hey, I&apos;m john 👋</h1>
 						<p className="prose prose-neutral dark:prose-invert text-muted-foreground">
-							I am a 23-year-old frontend developer from the Philippines with a passion in web development using{" "}
+							I am a 23-year-old frontend developer, and enthusiast photographer from the Philippines with a passion in
+							web development using{" "}
 							<Link
 								className={cn(
 									"border bg-background p-1 text-sm inline-flex items-center leading-4 no-underline rounded text-black dark:text-white",
@@ -35,26 +31,7 @@ export function HeroSection() {
 							>
 								<ReactIcon width={14} height={14} className="!mr-1 text-sky-400" /> React
 							</Link>
-							. My go-to stack for web development is{" "}
-							<Link
-								className={cn(
-									"border bg-transparent p-1 text-sm inline-flex items-center leading-4 no-underline rounded text-black dark:text-white",
-								)}
-								href="https://www.typescriptlang.org/"
-							>
-								<TypeScript width={14} height={14} className="!mr-1 text-blue-500" /> TypeScript
-							</Link>
-							, combined with the use of{" "}
-							<Link
-								className={cn(
-									"border bg-transparent p-1 leading-4 text-sm inline-flex items-center no-underline rounded text-black dark:text-white",
-								)}
-								href="https://www.postgresql.org/"
-							>
-								<PostgreSQL width={14} height={14} className="!mr-1 text-blue-400" /> PostgreSQL
-							</Link>
-							, an advanced, enterprise-class open-source relational database that supports both SQL (relational) and
-							JSON (non-relational) querying. Get in touch me via my socials below or check out my portfolio.
+							. Get in touch with me via my socials below or check out my portfolio.
 						</p>
 						<div className="flex flex-row items-start justify-start pt-4 pb-6 gap-2">
 							<Link href="/portfolio" className={cn(buttonVariants())}>
